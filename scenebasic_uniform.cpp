817d18803a5bf811b6523093d2da26d9fe196269
Jaminima <oscar.william.davies@outlook.com>
2023-02-02 11:59:15 +0000

    Make The Triangle Spin

--- scenebasic_uniform.cpp
+++ scenebasic_uniform.cpp
@@ -11,6 +11,8 @@ using std::cerr;
 using std::endl;
 
 #include "helper/glutils.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 using glm::vec3;
 
@@ -84,9 +86,12 @@ void SceneBasic_Uniform::compile()
 	}
 }
 
+glm::mat4 rotationMatrix;
+
 void SceneBasic_Uniform::update( float t )
 {
-	//update your angle here
+    //update your angle here
+    angle = (angle>360 || angle<0) ? 0 : angle + 0.1f;
 }
 
 void SceneBasic_Uniform::render()
@@ -94,6 +99,15 @@ void SceneBasic_Uniform::render()
     glClear(GL_COLOR_BUFFER_BIT);
     
     //create the rotation matrix here and update the uniform in the shader 
+
+
+    rotationMatrix = glm::rotate(glm::mat4(1.0f), angle, vec3(0.0f, 0.0f, 1.0f));
+
+    GLuint programHandle = prog.getHandle();
+    GLuint location = glGetUniformLocation(programHandle, "RotationMatrix");
+
+    glUniformMatrix4fv(location, 1, GL_FALSE, &rotationMatrix[0][0]);
+
 
     glBindVertexArray(vaoHandle);
     glDrawArrays(GL_TRIANGLES, 0, 3 );
