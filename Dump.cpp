e393f6d43682aa33a3bc802c6576df13f0413171
Oscar Davies <oscar.william.davies@outlook.com>
2023-03-24 11:08:46 +0000

    Independently Sized Vectors

--- Dump.cpp
+++ Dump.cpp
@@ -6,11 +6,6 @@
 #include<string>
 #include <algorithm>
 
-template<typename T>
-inline void DumpVertex(ofstream* fileStr, std::vector<T>* vec) {
-	fileStr->write((char*)vec->data(), vec->size() * sizeof(T));
-}
-
 inline string GetFormattedLengthString(int length) {
 	string old_str = to_string(length);
 	const size_t n_zero = 8;
@@ -20,6 +15,14 @@ inline string GetFormattedLengthString(int length) {
 	return string(new_str);
 }
 
+template<typename T>
+inline void DumpVertex(ofstream* fileStr, std::vector<T>* vec) {
+	string len = GetFormattedLengthString(vec->size());
+	fileStr->write(len.c_str(), len.size());
+
+	fileStr->write((char*)vec->data(), vec->size() * sizeof(T));
+}
+
 void Mesh::Dump(ofstream* fileStr)
 {
 	string nameLen = GetFormattedLengthString(name.size());
@@ -27,30 +30,36 @@ void Mesh::Dump(ofstream* fileStr)
 
 	fileStr->write(name.c_str(), name.size());
 
-	string len = GetFormattedLengthString(this->data->vertexSet.size());
-	fileStr->write(len.c_str(), len.size());
-
 	DumpVertex(fileStr, &this->data->vertexSet);
 	DumpVertex(fileStr, &this->data->texCooSet);
 	DumpVertex(fileStr, &this->data->normalSet);
 
-	printf("Dumped Mesh %s With %s elements\n", name.c_str(), len.c_str());
+	string total = to_string(this->data->vertexSet.size() + this->data->texCooSet.size() + this->data->normalSet.size());
+
+	printf("Dumped Mesh %s With %s elements\n", name.c_str(), total.c_str());
 }
 
 
 template<typename T>
-void BuildVector(vector<T>* vec, char* data, int dLen) {
+int BuildVector(vector<T>* vec, char* data, int idx) {
+	string lenStr;
+	memcpy(lenStr.data(), &data[idx], 8);
+
+	int dLen = stoi(lenStr) * sizeof(T);
+
 	int i = 0;
 
 	while (i < dLen) {
 		T vect;
 
-		memcpy(&vect, &data[i], sizeof(T));
+		memcpy(&vect, &data[idx + 8 + i], sizeof(T));
 
 		vec->push_back(vect);
 
 		i += sizeof(T);
 	}
+
+	return dLen + 8;
 }
 
 bool Asset::TryLoadDump()
@@ -58,7 +67,6 @@ bool Asset::TryLoadDump()
 	string filePath = srcFile + ".dump";
 
 	ifstream file(filePath, ios::binary | ios::ate);
-	string str;
 	if (file) {
 		int sz = file.tellg();
 		file.seekg(0);
@@ -86,30 +94,14 @@ bool Asset::TryLoadDump()
 
 			idx += len;
 
-			memcpy(lenStr.data(), &str_buff[idx], 8);
-			len = stoi(lenStr);
-
-			idx += 8;
-
-			int vec3len = len * sizeof(vec3);
-			int vec2len = len * sizeof(vec2);
-
 			Mesh m(name);
 			m.data = new MeshData();
 
-			char* vecData = new char[vec3len];
+			idx += BuildVector(&m.data->vertexSet, str_buff, idx);
 
-			memcpy(vecData, &str_buff[idx], vec3len);
-			BuildVector(&m.data->vertexSet, vecData, vec3len);
-			idx += vec3len;
+			idx += BuildVector(&m.data->texCooSet, str_buff, idx);
 
-			memcpy(vecData, &str_buff[idx], vec2len);
-			BuildVector(&m.data->texCooSet, vecData, vec2len);
-			idx += vec2len;
-
-			memcpy(vecData, &str_buff[idx], vec3len);
-			BuildVector(&m.data->normalSet, vecData,vec3len);
-			idx += vec3len;
+			idx += BuildVector(&m.data->normalSet, str_buff, idx);
 
 			this->meshses.push_back(m);
 		}
